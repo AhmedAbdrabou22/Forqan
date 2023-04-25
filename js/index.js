f4e050863ccf42c5e246b6935d53edf90a7851e3
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-04-25 15:03:15 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 const speechToken = document.querySelector('.speech')
 const search = document.querySelector('.search')
 
-speechToken.addEventListener('click',()=>{
+speechToken.addEventListener('click', () => {
     let recog = new webkitSpeechRecognition();
     recog.lang = "ar-SA";
-    recog.onresult = (eve)=>{
+    recog.onresult = (eve) => {
         search.value = eve.results[0][0].transcript;
     }
     recog.start()
 })
+
+
+let nav_item = document.querySelectorAll('.nav-item');
+nav_item.forEach((nav)=>{
+    nav.addEventListener('click',()=>{
+        nav_item.forEach((nav)=>{
+        nav.style.borderBottom = "none"
+    })
+        nav.style.borderBottom = '1px solid';
+    })
+})
+
+
+
+fetch('http://api.alquran.cloud/v1/quran/quran-uthmani')
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data.data.surahs[0].ayahs[0].text);
+    })
