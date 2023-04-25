c5e778581ccb07449e68e0fe404e33f384ca70f3
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-04-25 15:25:06 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -12,19 +12,36 @@ speechToken.addEventListener('click', () => {
 
 
 let nav_item = document.querySelectorAll('.nav-item');
-nav_item.forEach((nav)=>{
-    nav.addEventListener('click',()=>{
-        nav_item.forEach((nav)=>{
-        nav.style.borderBottom = "none"
-    })
+nav_item.forEach((nav) => {
+    nav.addEventListener('click', () => {
+        nav_item.forEach((nav) => {
+            nav.style.borderBottom = "none"
+        })
         nav.style.borderBottom = '1px solid';
     })
 })
 
 
 
+let content = document.querySelector('.content');
+console.log(content);
+
 fetch('http://api.alquran.cloud/v1/quran/quran-uthmani')
     .then(response => response.json())
     .then(data => {
-        // console.log(data.data.surahs[0].ayahs[0].text);
+        console.log(data.data.surahs.length);
+        content.innerHTML = "";
+        for (let i = 0; i <= data.data.surahs.length; i++) {
+            content.innerHTML += `
+            <div class="surah">
+            <div class="d-flex">
+                <div class="diamond">
+                    <p>${i+1}</p>
+                </div>
+                <div class="text_surah">
+                    <p>${data.data.surahs[i].name}</p>
+                </div>
+            </div>
+        </div>`
+        }
     })
