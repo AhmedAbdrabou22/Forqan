4a4f041767957091294ee6ae838ad2254c659c94
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-05-01 21:57:09 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -50,43 +50,84 @@ let ayat_pop = document.querySelector(".ayat-pop");
 let ayats = document.querySelector(".ayats");
 let audioAll = document.querySelector(".audio");
 let play_audio = document.getElementById('play');
-let pause_audio = document.getElementById('pause') 
-
+let pause_audio = document.getElementById('pause')
+const audio = new Audio();
 function fun(numOfsurah) {
     console.log(numOfsurah);
     ayat_pop.classList.add("active");
     fetch(`http://api.alquran.cloud/v1/surah/${numOfsurah}`)
         .then(response => response.json())
         .then(data => {
-            // console.log(data.data.ayahs);
             ayats.innerHTML = "";
             let dataAyat = data.data.ayahs;
-            dataAyat.forEach((aya) => {
-                ayats.innerHTML += `<p>(${aya.numberInSurah})-${aya.text}</p>`
-            })
-            // audio.setAttribute('src',`https://cdn.islamic.network/quran/audio-surah/128/ar.alafasy/${numOfsurah}.mp3`)
-            const audio = new Audio();
+            dataAyat.forEach((aya) => { ayats.innerHTML += `<p>(${aya.numberInSurah})-${aya.text}</p>` })
             audio.addEventListener('canplay', function () {
-                play_audio.addEventListener("click",()=>{
-                    play_audio.style.display="none";
-                    pause_audio.style.display="block";
-                    audio.play()
+                play_audio.addEventListener("click", () => {
+                    play_audio.style.display = "none";
+                    pause_audio.style.display = "block";
+                    audio.play();
+                    var elem = document.getElementById("myBar");
+                    var width = 1;
+                    var id = setInterval(frame, audio.duration * 10);
+                    function frame() {
+                        if (width >= 100) {
+                            clearInterval(id);
+                        } else {
+                            width++;
+                            elem.style.width = width + '%';
+                        }
+                    }
                 })
             });
             audio.addEventListener('canplay', function () {
-                pause_audio.addEventListener("click",()=>{
-                    play_audio.style.display="block";
-                    pause_audio.style.display="none";
+                pause_audio.addEventListener("click", () => {
+                    play_audio.style.display = "block";
+                    pause_audio.style.display = "none";
                     audio.pause()
                 })
             });
 
             // تعيين src الخاص بعنصر الصوت باستخدام setAttribute
             audio.setAttribute('src', `https://cdn.islamic.network/quran/audio-surah/128/ar.alafasy/${numOfsurah}.mp3`);
+            var duration = audio.duration
+            audio.addEventListener("loadedmetadata", () => {
+                console.log(audio.duration);
+            })
+
+            let close = document.querySelector(".close");
+            close.addEventListener("click", () => {
+                ayat_pop.classList.remove("active")
+                ayat_pop.classList.remove("active2");
+                audio.pause();
+                play_audio.style.display = "block";
+                pause_audio.style.display = "none";
+            })
         })
-    let close = document.querySelector(".close");
-    close.addEventListener("click", () => {
-        ayat_pop.classList.remove("active")
-        ayat_pop.classList.remove("active2")
-    })
 }
+
+
+
+
+
+
+
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 510);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+
+
+
+
+
